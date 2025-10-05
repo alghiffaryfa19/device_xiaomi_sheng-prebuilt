0e9c5a9cc2563c6404b4ca88c2caec1aa5f313fe
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2025-10-05 21:25:37 +0700

    wip

--- BoardConfigPrebuilt.mk
+++ BoardConfigPrebuilt.mk
@@ -6,16 +6,6 @@ AB_OTA_PARTITIONS += \
     vendor_boot \
     vendor_dlkm
 
-# Kernel (prebuilt)
-BOARD_PREBUILT_DTBOIMAGE := $(PREBUILT_PATH)/images/dtbo.img
-
-# Kernel modules (prebuilt)
-RAMDISK_MODULES_PATH := $(PREBUILT_PATH)/modules/ramdisk
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(RAMDISK_MODULES_PATH)/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load.recovery))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(RAMDISK_MODULES_PATH)/modules.blocklist
-
 # Partitions (prebuilt)
 BOARD_PREBUILT_ODMIMAGE := $(PREBUILT_PATH)/images/odm.img
 BOARD_PREBUILT_VENDORIMAGE := $(PREBUILT_PATH)/images/vendor.img
